c78d6f98b9823f432ec4550f35529937746be1bf
vortex2jm <joaopaulomoura5328@gmail.com>
2022-11-14 22:49:57 -0300

    :recycle: Refactoring candidate class

--- src/Candidate.java
+++ src/Candidate.java
@@ -2,7 +2,6 @@ import java.util.Date;
 
 public class Candidate implements Comparable<Candidate>{
     
-
     private int nrCandidato;
     private String nmUrnaCandidato;
     private Date dtNascimento;
@@ -11,7 +10,29 @@ public class Candidate implements Comparable<Candidate>{
     private PoliticalParty party;
     private int qtVotos;
 
+    public Candidate(int nrCandidato, String nmUrnaCandidato, Date dtNascimento,
+     Boolean cdSitTotTurno, int cdGenero, PoliticalParty party){
+
+        this.nrCandidato = nrCandidato;
+        this.nmUrnaCandidato = nmUrnaCandidato;
+        this.dtNascimento = dtNascimento;
+        this.cdSitTotTurno = cdSitTotTurno;
+        this.cdGenero = cdGenero;
+        this.party = party;
+        this.qtVotos = 0;
+    }
+
+
+    //===============Setters================================================//
+    public void setQtVotos(int qtVotos) {
+        this.qtVotos = qtVotos;
+    }
+
+
     
+
+
+
     //==============================Override methods========================//
     //@Override
     //public String toString() {
