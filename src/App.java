ff4e2ca0c8e7730de7d016139231b124d1a73b4a
vortex2jm <joaopaulomoura5328@gmail.com>
2022-11-15 03:07:29 -0300

    :sparkles: testing outputs

--- src/App.java
+++ src/App.java
@@ -1,16 +1,40 @@
 import java.io.BufferedReader;
-
+import java.util.List;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
         int type = Services.validateArgs(args[0]);
         BufferedReader bufferCandidates = Services.createBuffer(args[1]);
-        //BufferedReader bufferParties = Services.createBuffer(args[2]);
+        BufferedReader bufferVotes = Services.createBuffer(args[2]);
 
         Election election2022 = new Election();
 
         Services.processCandidatesFile(bufferCandidates, election2022, type);
+        Services.processVotesFile(bufferVotes, election2022, type);
+
+
+        // Testando as saídas
+        System.out.println(election2022.getNominalVotes());
+        System.out.println(election2022.getLegendVotes());
+        System.out.println(election2022.electedAmount());
+
+        List<Candidate> elctdCand = election2022.electedCandidates();
+        
+        int men =0;
+        int women =0;
+        int x=1;
+        for(Candidate c: elctdCand){
+            System.out.printf("%d - ",x);
+            System.out.println(c);
+            x++;
+            if(c.getCdGenero() == 2)
+                men++;
+            if(c.getCdGenero() == 4)
+                women++;
+        }
+        System.out.println(men);
+        System.out.println(women);        
     }
 }
 
@@ -32,12 +56,3 @@ public class App {
 // cdCargoPart;
 // nrVotavel;
 // qtVotos;
-
-//======Método para ver o indice da legenda
-//String[] firstLine = bufferCandidates.readLine().split(";");
-//int x=0;
-//for(String s: firstLine){
-//    System.out.printf("%d - ",x);
-//    System.out.println(s);
-//    x++;
-//}
