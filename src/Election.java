52ed8cfb2bd940b1bb99bea939def25cb9dec88c
vortex2jm <joaopaulomoura5328@gmail.com>
2022-11-15 03:09:17 -0300

    :sparkles: Implementing new methods

--- src/Election.java
+++ src/Election.java
@@ -1,11 +1,54 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Election {
     
+    // Separar os packages posteriormente
     // Estes dados poderão ser acessados diretamente somente pela classe Services
     protected Map<Integer, Candidate> candidates = new HashMap<>();
     protected Map<Integer, PoliticalParty> parties = new HashMap<>();
 
-    //Aqui ficarão os métodos que retornarão as estatísticas das eleições
+    private int nominalVotes = 0;
+    private int legendVotes = 0;
+    
+    //==============Getters======================//
+    public int getNominalVotes() {
+        return nominalVotes;
+    }
+    public int getLegendVotes() {
+        return legendVotes;
+    }
+
+    //==============Setters======================//
+    public void setNominalVotes(int nominalVotes) {
+        this.nominalVotes += nominalVotes;
+    }
+    public void setLegendVotes(int legendVotes) {
+        this.legendVotes += legendVotes;
+    }
+
+    //========================================================================//
+    public int electedAmount(){
+        int result = 0;
+        var c = new ArrayList<Candidate>(candidates.values());
+        for(Candidate cand: c){
+            if(cand.getCdSitTotTurno())
+                result++;
+        }
+        return result;
+    }   
+
+    public List<Candidate> electedCandidates(){
+        var c = new ArrayList<Candidate>(candidates.values());
+        var result = new ArrayList<Candidate>();
+        for(Candidate cand: c){
+            if(cand.getCdSitTotTurno()){
+                result.add(cand);
+            }
+        }
+        result.sort(null);
+        return result;
+    }
 }   
