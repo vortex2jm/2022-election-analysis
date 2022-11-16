da57c3d6b57f945eae260cd7a372a6829753bfa1
vortex2jm <joaopaulomoura5328@gmail.com>
2022-11-16 13:45:05 -0300

    :sparkles: Adding comparator

---
+++ src/br/ufes/edu/jh/util/comparators/PartyComparatorByCandidate.java
@@ -0,0 +1,26 @@
+package br.ufes.edu.jh.util.comparators;
+
+import java.util.Comparator;
+
+import br.ufes.edu.jh.domain.PoliticalParty;
+
+public class PartyComparatorByCandidate implements Comparator<PoliticalParty>{
+
+    @Override
+    public int compare(PoliticalParty arg0, PoliticalParty arg1) {
+        
+        if(arg0.mostVotedCandidate().getQtVotos() > arg1.mostVotedCandidate().getQtVotos())
+            return -1;
+        if(arg0.mostVotedCandidate().getQtVotos() < arg1.mostVotedCandidate().getQtVotos())
+            return 1;
+        if(arg0.mostVotedCandidate().getDtNascimento().compareTo(arg1.mostVotedCandidate().getDtNascimento()) < 0)
+            return -1;
+        if(arg0.mostVotedCandidate().getDtNascimento().compareTo(arg1.mostVotedCandidate().getDtNascimento()) > 0)
+            return 1;
+        if(arg0.mostVotedCandidate().getParty().getNumber() < arg1.mostVotedCandidate().getParty().getNumber())
+            return -1;
+        if(arg0.mostVotedCandidate().getParty().getNumber() < arg1.mostVotedCandidate().getParty().getNumber())
+            return 1;
+        return 0;
+    }    
+}
